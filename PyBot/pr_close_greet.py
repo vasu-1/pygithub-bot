71db65f7ba48d3d029315333eda944a58d1fc333
Vashishth Patel <vashishthchaudhary48@gmail.com>
2022-02-01 23:39:06 +0530

    added pr close

--- PyBot/pr_close_greet.py
+++ PyBot/pr_close_greet.py
@@ -14,7 +14,7 @@ async def issue_opened_event(event, gh, *args, **kwargs):
     ur = event.data['pull_request']['comments_url']
     
     #author of the issue creater
-    author = event.data['issue']['user']['login']
+    author = event.data['pull_request']['user']['login']
 
     # avatar = event.data['issue']['user']['avatar_url']
 
