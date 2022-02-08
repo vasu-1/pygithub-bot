4215b7d5ead543b17961bebe1c99ec52e516eb21
Vashishth Patel <vashishthchaudhary48@gmail.com>
2022-02-08 15:52:20 +0530

    deployment status added

--- PyBot/deployment_status.py
+++ PyBot/deployment_status.py
@@ -4,13 +4,14 @@ from gidgethub import aiohttp as gh_aiohttp
 
 router = routing.Router()
 
-link = "https://api.github.com/repos/vasu-1/testpython/issues/3/comments";
+# link = "https://api.github.com/repos/vasu-1/testpython/issues/3/comments";
 
 @router.register("workflow_run", action="completed")
 async def workflow_job(event, gh, *arg, **kwargs):
 
 	status = event.data['workflow_run']['conclusion']
 	# ur = event.data['pull_request']['comments_url']
+	link = event.data['workflow_run']['repository']['issue_comment_url']
 
 	if(status == "success"):
 		messag = f"succeed"
