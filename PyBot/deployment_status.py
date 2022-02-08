56fb7bc961803785e8bb997c85771a748fa04f6d
Vashishth Patel <vashishthchaudhary48@gmail.com>
2022-02-08 15:07:06 +0530

    deployment status added

---
+++ PyBot/deployment_status.py
@@ -0,0 +1,24 @@
+from gidgethub import routing, sansio
+from gidgethub import aiohttp as gh_aiohttp
+
+
+router = routing.Router()
+
+link = "https://api.github.com/repos/vasu-1/testpython/issues/3/comments";
+
+@router.register("deployment_status", action="")
+async def opened_pr(event, gh, *arg, **kwargs):
+
+    status = event.data['deployment_status']['state']
+    # ur = event.data['pull_request']['comments_url']
+
+    if(status == "success") :
+	    messag = f"succed"
+	    await gh.post(link, data={
+	        'body': messag,
+	        })
+	else if(status == "failure") :
+		messag = f"Failure"
+	    await gh.post(link, data={
+	        'body': messag,
+	        })
